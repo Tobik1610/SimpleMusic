80509e1476c229cf403054e4674c523700d53208
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-11-05 15:48:04 +0100

    Optimization

--- SimpleMusic/src/Sound.java
+++ SimpleMusic/src/Sound.java
@@ -1,13 +1,7 @@
 import java.io.File;
-import java.io.IOException;
-
-import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 
 public class Sound implements Runnable {
 
@@ -28,33 +22,14 @@ public class Sound implements Runnable {
 		AudioInputStream audioInputStream = null;
 		try {
 			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
 			Clip clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
 			clip.start();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		try {
 			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
 			audioInputStream.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 }
