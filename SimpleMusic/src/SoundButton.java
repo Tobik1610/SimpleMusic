a84232cb88efa90512878b54bee2d4b473d888e4
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-11-05 16:28:59 +0100

    UI

---
+++ SimpleMusic/src/SoundButton.java
@@ -0,0 +1,28 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class SoundButton extends JButton {
+
+	private Sound sound;
+
+	public SoundButton(Sounds sounds, MusicLoop ml) {
+		setPreferredSize(new Dimension(100, 100));
+		setText(sounds.name());
+
+		sound = new Sound(sounds);
+		
+		addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ml.addSound(ml.getBeat(), sound);
+				
+				sound.play();
+			}
+		});
+	}
+
+}
