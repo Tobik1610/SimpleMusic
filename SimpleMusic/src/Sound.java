244697d4d7631426bdcc5319d13942d0528877d7
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-11-05 15:42:25 +0100

    Sound improvement

--- SimpleMusic/src/Sound.java
+++ SimpleMusic/src/Sound.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
@@ -30,32 +31,30 @@ public class Sound implements Runnable {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		AudioFormat audioFormat = audioInputStream.getFormat();
-		SourceDataLine line = null;
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
 		try {
-			line = (SourceDataLine) AudioSystem.getLine(info);
-			line.open(audioFormat);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
 		} catch (LineUnavailableException e) {
 			e.printStackTrace();
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		line.start();
-		int nBytesRead = 0;
-		byte[] abData = new byte[128000];
-		while (nBytesRead != -1) {
-			try {
-				 nBytesRead = audioInputStream.read(abData, 0, abData.length);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			if (nBytesRead >= 0) {
-				 int nBytesWritten = line.write(abData, 0, nBytesRead);
-			}
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		line.drain();
-		line.close();
+		
+		try {
+			audioInputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
