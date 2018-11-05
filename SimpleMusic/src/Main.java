d0a3ea86a876429019323d51ac987fee9cbbaddb
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-11-05 17:08:41 +0100

    V0.1

--- SimpleMusic/src/Main.java
+++ SimpleMusic/src/Main.java
@@ -4,31 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		new MainFrame();
-		
-//		MusicLoop ml = new MusicLoop(120, 4, true);
-//		
-//		Sound kick = new Sound(Sounds.KICK);
-//		Sound snare = new Sound(Sounds.SNARE);
-//		Sound closed_hh = new Sound(Sounds.CLOSED_HIHAT);
-//		
-//		ml.addSound(2, kick);
-//		ml.addSound(4, kick);
-//		ml.addSound(6, kick);
-//		ml.addSound(8, kick);
-//		ml.addSound(10, kick);
-//		ml.addSound(12, kick);
-//		ml.addSound(14, kick);
-//		ml.addSound(16, kick);
-//		
-//		ml.addSound(8, snare);
-//		ml.addSound(16, snare);
-//
-//		ml.addSound(6, closed_hh);
-//		ml.addSound(7, closed_hh);
-//		ml.addSound(14, closed_hh);
-//		ml.addSound(15, closed_hh);
-//		
-//		ml.start();
+
 	}
 
 }
